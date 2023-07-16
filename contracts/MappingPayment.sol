c51e4e64d474e3069d9cf995e4b617d444a50e85
ayushtiwari134 <ayushtiwari134@gmail.com>
2023-07-16 19:29:54 +0530

    modified:   contracts/MappingPayment.sol

--- contracts/MappingPayment.sol
+++ contracts/MappingPayment.sol
@@ -2,21 +2,25 @@
 pragma solidity ^0.8.0;
 
 contract Practice {
+    // Creating a struct which basically defines a person.
     struct Person {
         string name;
         address payable receiver;
     }
 
+    // Creating a mapping to categorise each person and call them with a specific id.
     mapping(uint256 => Person) id;
 
+    // Function to create a mapping using an id, name and address of the person.
     function createPersonMapping(
         uint _id,
         string memory _name,
         address payable _receiver
     ) public {
-        id[_id] = Person(_name, _receiver);
+        id[_id] = Person(_name, _receiver); // id[index]=>Person() struct.
     }
 
+    // Function to view a particular person on the basis of an id that is taken as input.
     function getPerson(
         uint _id
     ) public view returns (string memory _name, address _receiver) {
