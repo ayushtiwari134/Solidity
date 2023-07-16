9c3c38461d9f1a5cc8fcdd75cbf8cee8b4d79805
ayushtiwari134 <ayushtiwari134@gmail.com>
2023-07-17 03:09:59 +0530

    new file:   contracts/Lottery.sol

---
+++ contracts/Lottery.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Lottery {
+    address public owner;
+    uint playerCount = 0;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    struct Player {
+        string name;
+        address payable player;
+    }
+
+    Player[] public players;
+
+    function getLength() public view returns (uint) {
+        return players.length;
+    }
+
+    function createPlayer(string memory _name) public payable {
+        require(msg.value >= 1 ether);
+        Player memory p;
+        p.name = _name;
+        p.player = payable(msg.sender);
+        players.push(p);
+        playerCount++;
+    }
+
+    function createRandomNumber() public view returns (uint) {
+        require(msg.sender == owner);
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(
+                        msg.sender,
+                        block.timestamp,
+                        players.length
+                    )
+                )
+            );
+    }
+
+    function selectWinner() public {
+        require(msg.sender == owner);
+        uint index = (createRandomNumber()) % (players.length);
+        uint balance = address(this).balance;
+        players[index].player.transfer(balance);
+        playerCount = 0;
+    }
+}
